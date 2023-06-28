6a53d5412bc48112dec0e1a1e24280bf58b68ae7
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-28 16:54:16 -0700

    added mock data for users & user schema,

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+//a schema that is created for the user that will represent a model of the data
+
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 100,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique:true,
+            max: 50,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 5,
+            max: 100,
+        },
+        city: {
+            type: String,
+           
+        },
+        state: String,
+        country: String,
+        occupation: String,
+        phoneNumber: String,
+        transactions: Array,
+        role: {
+            type: String,
+            enum:["user", "admin", "superadmin"],
+            default: "admin"
+        },        
+    },
+    {timestamps : true}
+)
+
+const User = mongoose.model("User", UserSchema);
+export default User;
