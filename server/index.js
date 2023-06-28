35cbf31912310d9fd75a9795ec0be136eadbf2aa
tarnaldo88 <tarnaldo88@gmail.com>
2023-06-28 15:33:52 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,3 +26,14 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managmentRoutes);
 app.use("/sales", salesRoutes);
+
+//mongoose setup
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => {
+    console.log(`${error} did not connect`)
+})
