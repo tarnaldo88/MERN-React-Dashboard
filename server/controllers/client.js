866f8b8f7c217d5052ec4022e81472dab13faf6b
tarnaldo88 <tarnaldo88@gmail.com>
2023-07-06 16:36:33 -0700

    Update client.js

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,1 +1,21 @@
-import Product from "../models/Product";
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async(req,res)  => {
+    try{
+        const products = await Product.find();
+
+        const productsWithStats = await Promise.all(
+            products.map(async (product) => {
+                const stat = await ProductStat.find({
+                    productId: product._id
+                })
+                return {
+                    ...product._doc,
+                }
+            })
+        );
+    } catch (error){
+        res.status(404).json({message: error.message});
+    }
+}
